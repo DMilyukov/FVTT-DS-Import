0b27beac9ffa2749cfb1f57bc2696d96ee9d2dfa
DMilyukov <77637266+DMilyukov@users.noreply.github.com>
2021-01-18 19:56:39 +0100

    Create dsimport.js

---
+++ dsimport.js
@@ -0,0 +1,31 @@
+Hooks.on("renderSidebarTab", async (app, html) => {
+  if (app.options.id == "scenes") {
+    let button = $("<button class='import-dd'><i class='fas fa-file-import'></i> Dungeondraft Import</button>")
+ 
+    button.click(function () {
+      //nothing yet
+    });
+    
+    html.find(".directory-footer").append(button);
+  }
+})
+
+Hooks.on("init", () => {
+  game.settings.register("ds-import", "importSettings", {
+    name: "Dungeon Scrawl Default Path",
+    scope: "world",
+    config: false,
+    default: {
+      source: "data",
+      extension: "png",
+      bucket: "",
+      region: "",
+      path: "worlds/" + game.world.name,
+      offset: 0.1,
+      fidelity: 3,
+      multiImageMode: "g",
+      webpConversion: true,
+      wallsAroundFiles: true,
+    }
+  })
+})
